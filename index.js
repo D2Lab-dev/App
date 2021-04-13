5674b9219d6ffcbf48eda1a11d10a38ba52001ff
marco bassoli <dotalab.unipr@gmail.com>
2021-04-13 12:11:53 +0200

    Test correct redirection after PIN login.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Operator login check
 $("#operatorLoginBtn").click(() => {
   if ($("#operatorPINInput").val() === "0000") {
-    window.location.href = "/newCalibration.html"
+    window.location.href = window.location.href + "/newCalibration.html"
   } else {
     $("#operatorLogin").modal('toggle');
     $("#wrongPIN").modal('toggle');
@@ -11,7 +11,7 @@ $("#operatorLoginBtn").click(() => {
 // Technical login check
 $("#technicalLoginBtn").click(() => {
   if ($("#technicalPINInput").val() === "9999") {
-    window.location.href = "/newAnalysis.html"
+    window.location.href = window.location.href + "/newAnalysis.html"
   } else {
     $("#technicalLogin").modal('toggle');
     $("#wrongPIN").modal('toggle');
