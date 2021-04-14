64454f2e2300eb2307f3e329cd0e4f890f398347
marco bassoli <dotalab.unipr@gmail.com>
2021-04-14 12:47:17 +0200

    Review login screen.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// Operator login check
-$("#operatorLoginBtn").click(() => {
-  if ($("#operatorPINInput").val() === "0000") {
+// User login check
+$("#userLoginBtn").click(() => {
+  if ($("#userPINInput").val() === "0000") {
     window.location.href = window.location.href.split("index.html")[0] + "/newCalibration.html"
   } else {
-    $("#operatorLogin").modal('toggle');
+    $("#userLogin").modal('toggle');
     $("#wrongPIN").modal('toggle');
   }
 });
 
-// Technical login check
-$("#technicalLoginBtn").click(() => {
-  if ($("#technicalPINInput").val() === "9999") {
+// Technician login check
+$("#technicianLoginBtn").click(() => {
+  if ($("#technicianPINInput").val() === "0000") {
     window.location.href = window.location.href.split("index.html")[0] + "/newAnalysis.html"
   } else {
-    $("#technicalLogin").modal('toggle');
+    $("#technicianLogin").modal('toggle');
     $("#wrongPIN").modal('toggle');
   }
 });
