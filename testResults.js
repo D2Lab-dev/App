32808698d5d89d4eec3f490333c346108c467dba
marbasso <marco@SkynetArch.localdomain>
2021-05-24 12:47:27 +0200

    Add auto-update of test result page.

--- testResults.js
+++ testResults.js
@@ -2,8 +2,9 @@
 const queryString = "https://api.thingspeak.com/channels/814496/feeds.json?api_key=1PKL71LY43MFGVMI&results=100";
 var tests = new Map(); // Create Map object to hold data (acts like a local database)
 
-// Get data from Thingspeak
-fetch(queryString)
+function pollResultsUpdate() {
+  // Get data from Thingspeak
+  fetch(queryString)
   .then(response => response.json())
   .then(data => {
     // Collect downloaded data using "DeviceID" as the key value
@@ -17,6 +18,7 @@ fetch(queryString)
         }
         // Update "tests" data (overwrite until last one - i.e. the most recent for each DeviceID)
         tests.set(feed.field5, {
+          timestamp: feed.created_at,
           typeOfAnalysis: feed.field7,
           CF: feed.field6,
           result: feed.field8
@@ -24,28 +26,44 @@ fetch(queryString)
       }
     });
   });
+  updateResult();
+}
 
-// Act on the selection list
-$("#selectDeviceList").change(() => {
+function updateResult() {
   // Check if the selection exists inside "tests"
   if (tests.has($("#selectDeviceList").val())) {
     // Retrieve test data
     let test = tests.get($("#selectDeviceList").val());
     // Show data
+    // Pretty print timestamp
+    let timestampPrint = test.timestamp;
+    timestampPrint = timestampPrint.replace("T"," "); // Replace "T" with a space
+    timestampPrint = timestampPrint.replace("Z",""); // Remove "Z"
+    $("#timestampField").text(timestampPrint);
     $("#typeOfAnalysisField").text(test.typeOfAnalysis);
     $("#CFField").text(test.CF);
     // Show positive/negative image
     if (test.result === "true") {
       $(".test-result-image-positive").attr("src", "images/red-circle-fill.svg");
+      $(".test-result-image-negative").attr("src", "images/grey-circle-fill.svg");
     } else {
+      $(".test-result-image-positive").attr("src", "images/grey-circle-fill.svg");
       $(".test-result-image-negative").attr("src", "images/green-circle-fill.svg");
     }
   } else {
     // Fill with default values
+    $("#timestampField").text("-");
     $("#typeOfAnalysisField").text("-");
     $("#CFField").text("-");
     $("#resultField").text("-");
     $(".test-result-image-positive").attr("src", "images/grey-circle-fill.svg");
     $(".test-result-image-negative").attr("src", "images/grey-circle-fill.svg");
   }
+}
+
+// Act on the selection list
+$("#selectDeviceList").change(() => {
+  updateResult();
 });
+
+setInterval(pollResultsUpdate, 2000);
